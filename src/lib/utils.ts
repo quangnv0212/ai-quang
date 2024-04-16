07dc46fb836fdc8e0be9d4e395ea0180f1378007
Vu Quang <quangnv.0212@gmail.com>
2024-04-16 09:19:28 +0700

    commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,38 +1,11 @@
-import { EntityError } from "@/lib/http";
-import { type ClassValue, clsx } from "clsx";
-import { UseFormSetError } from "react-hook-form";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx";
 import jwt from "jsonwebtoken";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const handleErrorApi = ({
-  error,
-  setError,
-  duration,
-}: {
-  error: any;
-  setError?: UseFormSetError<any>;
-  duration?: number;
-}) => {
-  if (error instanceof EntityError && setError) {
-    // error.payload.errors.forEach((item) => {
-    //   setError(item.field, {
-    //     type: "server",
-    //     message: item.message,
-    //   });
-    // });
-  } else {
-    // toast({
-    //   title: "Lỗi",
-    //   description: error?.payload?.message ?? "Lỗi không xác định",
-    //   variant: "destructive",
-    //   duration: duration ?? 5000,
-    // });
-  }
-};
 /**
  * Xóa đi ký tự `/` đầu tiên của path
  */
