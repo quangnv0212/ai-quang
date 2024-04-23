27c1c8c57cf66e94999d1c649291fbb60c4104c3
Vu Quang <quangnv.0212@gmail.com>
2024-04-23 15:01:05 +0700

    update

--- src/app/account-management/page.tsx
+++ src/app/account-management/page.tsx
@@ -1,18 +1,9 @@
 "use client";
-import { useAppContext } from "@/app-provider";
-import { PermissionDenied } from "@/components/permission-denied";
 import TableUser from "@/components/table-user";
-import { useEffect, useState } from "react";
 export default function UserManagementPage() {
-  const context = useAppContext();
-  const checkPermission = context.user?.permissions?.includes("Pages.Users");
-  const [isClient, setIsClient] = useState(false);
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
   return (
     <div>
-      {!isClient || !checkPermission ? <PermissionDenied /> : <TableUser />}
+      <TableUser />
     </div>
   );
 }
